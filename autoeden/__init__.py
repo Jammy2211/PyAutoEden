87347d9c751e91730c5e3af88199ff00bce8d51c
Richard <richardhayes777@gmail.com>
2022-10-28 12:35:04 +0100

    check python version

--- autoeden/__init__.py
+++ autoeden/__init__.py
@@ -9,28 +9,23 @@ from .line import LineItem
 from .package import Package
 
 
+from sys import version_info
+
+if version_info >= (3, 10):
+    print("Python versions greater than 3.9 not supported")
+    exit(1)
+
+
 def edenise(
-        root_directory,
-        package,
-        target_path=None,
-        eden_path=None,
+    root_directory, package, target_path=None, eden_path=None,
 ):
     target_path = target_path or f"{os.getcwd()}/../build_target/{package.name}_eden"
     eden_path = eden_path or f"{os.getcwd()}/../build_eden/{package.prefix}"
 
     print(f"Creating {target_path}...")
-    shutil.rmtree(
-        target_path,
-        ignore_errors=True
-    )
-    shutil.copytree(
-        root_directory,
-        target_path,
-        symlinks=True
-    )
+    shutil.rmtree(target_path, ignore_errors=True)
+    shutil.copytree(root_directory, target_path, symlinks=True)
 
     eden_path = Path(eden_path)
 
-    package.generate_target(
-        eden_path
-    )
+    package.generate_target(eden_path)
